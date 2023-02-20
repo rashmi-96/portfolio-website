e76d7841c925b60ee5bc26aefc96f44f0019d72d
rashmi-96 <97506841+rashmi-96@users.noreply.github.com>
2023-02-20 09:49:32 +0530

    commit new changes

--- script.js
+++ script.js
@@ -37,3 +37,24 @@ window.addEventListener("scroll", () =>{
     var element = document.body;
     element.classList.toggle('dark-mode')
   }
+
+  //scroll reveal animation//
+  
+  const animation = ScrollReveal({
+    origin:"top",
+    distance:"80px",
+    duration:2000,
+    reset:true,
+
+  });
+/*home page*/
+  animation.reveal(".Intro-text",{});
+  animation.reveal(".button",{delay:200});
+  animation.reveal(".social-conatiner",{delay:100});
+  animation.reveal(".home-image",{interval:200});
+
+  /*about*/
+  animation.reveal(".about-title",{});
+  animation.reveal(".about-img",{delay:400});
+  animation.reveal(".about-text",{delay:400});
+  
