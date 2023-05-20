b283530cfd431c293154955fb1dcdafcc20e25fd
rashmi-96 <97506841+rashmi-96@users.noreply.github.com>
2023-05-20 23:22:05 +0530

    scroll top

--- script.js
+++ script.js
@@ -60,7 +60,25 @@ function toggleMenu(){
 
 // skill section animation
 
+//scroll to the top button
+let topbutton = document.getElementById("top-btn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollTopFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
   
 
